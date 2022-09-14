2829a72171db539aeb3ca1301820ef75dc8c6cdd
Simon <sajforbes@gmail.com>
2022-09-14 21:39:51 +0100

    Google 7

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,18 +2,18 @@ var store = [{
         "title": "Why Adults Make Mistakes When Children Don't",
         "excerpt":"Why do children seem to have a natural ability to learn languages perfectly that adults lack? Are their brains inherently better at language learning than us adults, or are they better positioned to take advantage of a natural ability we all possess? Wherever you fall, most people tend to say...","categories": ["blog"],
         "tags": ["language learning"],
-        "url": "/blog/why-adults-make-mistakes-when-children-dont/",
+        "url": "/pages/sajforbes/blog/why-adults-make-mistakes-when-children-dont/",
         "teaser": null
       },{
         "title": "Why Language Learning Takes So Long, And What To Do About It - Part 1",
         "excerpt":"Language learning takes a long time, usually much longer than any learner anticipates when they begin. At first, it seems pretty easy. Most people start with a bang, putting in the hours and achieving a basic ability in a few months. From there, everything seems to slow down. Suddenly, improving...","categories": ["blog"],
         "tags": ["language learning"],
-        "url": "/blog/why-language-learning-takes-so-long-and-what-to-do-about-it/",
+        "url": "/pages/sajforbes/blog/why-language-learning-takes-so-long-and-what-to-do-about-it/",
         "teaser": null
       },{
         "title": "LanguageLearning Subreddit Survey Exploratory Analysis",
         "excerpt":"Welcome to this analysis of the /r/languagelearning subreddit survey data. Before I begin I should mention a bit about myself. In addition to my role as the moderator of the /r/languagelearning subreddit on reddit, I am also the author of the ebook How to Teach Yourself a Foreign Language, which...","categories": ["dataanalysis"],
         "tags": ["Language learning","Subreddit","Survey","Data"],
-        "url": "/dataanalysis/LanguageLearning-Subreddit-Survey-Exploratory-Analysis/",
+        "url": "/pages/sajforbes/dataanalysis/LanguageLearning-Subreddit-Survey-Exploratory-Analysis/",
         "teaser": null
       }]
